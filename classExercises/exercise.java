9cd11df2e37ce74de67d0bccedcf3f6689add2e5
Rasira <davikmas@gmail.com>
2016-10-02 12:41:53 +0200

    exercise class initialized

---
+++ classExercises/exercise.java
@@ -0,0 +1,22 @@
+//Class implementation of an exercise
+//TODO: 
+//    - Currently shown indicator
+//    - Solution field
+//    - Previously recorded time taken
+//    - more stuff?
+//    - Type of problem (Read code or read output?)
+public class exercise
+{ 
+	private boolean shown;
+	private String taskText;
+	private double recordedTimeTakenToSolve;
+	
+        void exercise () {
+		shown=false;
+		taskText="No task has been loaded";
+		recordedTimeTakenToSolve=-1;
+	}
+	
+	void exercise (
+
+}
