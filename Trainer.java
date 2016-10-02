2f197a8ef6db4b3d493af3a48cd76dd71e2d4143
Rasira <davikmas@gmail.com>
2016-10-02 14:26:45 +0200

    somerev

--- Trainer.java
+++ Trainer.java
@@ -3,7 +3,7 @@ public class Trainer
 
 	public static void main(String [] args)
 	{
-
+		//Added comment 
 		System.out.println("ehm");
 	    	System.out.println("eh");
 		System.out.println("many rawrs will be made");
